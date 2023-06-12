eb11ce2188b1689de498ad980e15abe87aac7b72
chochonoya <adalilovna1@gmail.com>
2023-06-12 21:18:50 +0600

    finish git user

---
+++ script.js
@@ -0,0 +1,22 @@
+const API = "https://api.github.com/users/";
+
+const search = document.getElementById("search");
+
+const img = document.getElementById("img");
+const idUser = document.getElementById("idUser");
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const repositories = document.getElementById("repositories");
+
+async function getUsersByTag(event) {
+  event.preventDefault();
+  const response = await fetch(API + search.value);
+  const users = await response.json();
+  console.log("users: ", users);
+
+  img.src = users.avatar_url;
+  idUser.innerHTML = `id: ${users.id}`;
+  followers.innerHTML = `followers: ${users.followers}`;
+  following.innerHTML = `following: ${users.following}`;
+  repositories.innerHTML = `repositories: ${users.public_repos}`;
+}
